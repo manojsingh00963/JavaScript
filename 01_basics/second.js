68ce1c8e577638434d8936540567a3266c5e313d
manoj singh <155624353+manojsingh00963@users.noreply.github.com>
2024-07-20 07:28:03 +0000

    container config done

---
+++ 01_basics/second.js
@@ -0,0 +1,10 @@
+let hello = 'Hello world!';
+
+let message;
+
+// copy 'Hello world' from hello into message
+message = hello;
+
+// now two variables hold the same data
+console.log(hello); // Hello world!
+console.log(message); // Hello world!
