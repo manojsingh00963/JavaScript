194a57ebf65ed7b725f12a7ff6f332057b9370b9
manojsingh00963 <manojsingh00963@gmail.com>
2024-07-30 10:51:30 +0530

    "Congratulation For first java Script"

---
+++ 01_basics/01_first.js
@@ -0,0 +1,13 @@
+console.log("Hello World")
+console.log(" Congratulations you have done successfully!")
+
+let hello = 'Hello world!';
+
+let message;
+
+// copy 'Hello world' from hello into message
+message = hello;
+
+// now two variables hold the same data
+console.log(hello); // Hello world!
+console.log(message); // Hello world!
