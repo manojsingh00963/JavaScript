a24915d1635f0e1aedb961a57e9364b99e91d22b
manojsingh00963 <manojsingh00963@gmail.com>
2024-08-09 21:25:57 +0530

    "Color changer"

---
+++ Project's/color changer/script.js
@@ -0,0 +1,21 @@
+function bggrey(){
+    document.querySelector('#grey').addEventListener('click',()=>{
+        document.body.style.backgroundColor = "grey";})
+        document.body.style.color = "beige";
+}
+function bgwhite(){
+    document.querySelector('#white').addEventListener('click',()=>{
+        document.body.style.backgroundColor = "white";})
+        document.body.style.color = "black";
+}
+function bgblue(){
+    document.querySelector('#blue').addEventListener('click',()=>{
+        document.body.style.backgroundColor = "blue";})
+        document.body.style.color = "white";
+}
+function bgyellow(){
+    document.querySelector('#yellow').addEventListener('click',()=>{
+        document.body.style.backgroundColor = "yellow";
+        document.body.style.color = "red";
+    })
+}
