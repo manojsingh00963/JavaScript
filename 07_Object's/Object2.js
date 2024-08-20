cf83df7fbf410048b71e4ee96f49252864dcf009
manoj singh <manojsingh00963@gmail.com>
2024-08-20 15:01:49 +0530

    'object assign'

--- 07_Object's/Object2.js
+++ 07_Object's/Object2.js
@@ -25,3 +25,45 @@ const pl2= new playerboy("Lakshit", "Varma", "Banglore", false);
 // console.log(pl1.player)
 
 // console.log(pl2.fullname() + " are from " + pl2.address + " and he is a player " + pl2.player)
+
+
+// Object Assign
+
+let widget = {
+    color:'red',
+}
+
+const bgcolor = Object.assign({},widget)
+
+console.log(bgcolor)
+
+
+// Object.assign Merge 2 Object.
+
+let box = {
+    height:'10px',
+    weight:'20px'
+}
+
+let style = {
+    color:'red',
+    borderStyle:'solid'
+}
+
+const boxStyle = Object.assign({},box,style)
+
+console.log(boxStyle)
+
+let Science_student = {
+    s1:'Rohan',
+    c2:'Science'
+}
+
+let Math_student = {
+    s2:'Lisha',
+    c1:'Math'
+}
+
+const bestStudent = Object.assign(Science_student,Math_student)
+// use curly braces not complesary javascript auto maticaly use but for good practice should use,
+// const bestStudent = Object.assign({},Science_student,Math_student)
